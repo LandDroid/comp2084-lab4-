93ea39ccb8892c28668a9adeabeac7ed5d7f9f51
Landon Lyle <landonlyle@live.ca>
2020-01-31 15:43:52 -0500

    Completing Lab 4

---
+++ PhoneStoreApp/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhoneStoreApp.Models
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Phone> Phone { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+    }
+}
---
+++ PhoneStoreApp/Models/Phone.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PhoneStoreApp.Models
+{
+    public class Phone
+    {
+        [Key]
+        public int PhoneID { get; set; }
+        public string PhoneName { get; set; }
+        public virtual Manufacturer Manufacturer { get; set; }
+        public int MSRP { get; set; }
+        public string ScreenSize { get; set; }
+        public DateTime DateReleased { get; set; }
+        [ForeignKey("Manufacturer")]
+        public int ManufacturerID { get; set; }
+
+        public Phone(int phoneID, string phoneName, int mSRP, string screenSize, DateTime dateReleased, int manufacturerID)
+        {
+            PhoneID = phoneID;
+            PhoneName = phoneName;
+            MSRP = mSRP;
+            ScreenSize = screenSize;
+            DateReleased = dateReleased;
+            ManufacturerID = manufacturerID;
+        }
+
+        public Phone(string phoneName, int mSRP, DateTime dateReleased, int manufacturerID)
+        {
+            PhoneName = phoneName;
+            MSRP = mSRP;
+            DateReleased = dateReleased;
+            ManufacturerID = manufacturerID;
+        }
+
+        public Phone() { }
+    }
+}
